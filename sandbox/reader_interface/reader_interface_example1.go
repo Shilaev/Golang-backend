159d2452466181b6c897ab3aec74687b496cbac7
Shilaev Valentin <valentin.shilaev@yandex.ru>
2023-01-09 20:01:22 +0300

    udpate

--- sandbox/reader_interface/reader_interface_example1.go
+++ sandbox/reader_interface/reader_interface_example1.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"strings"
+)
 
+func main() {
+	myReader := io.Reader(strings.NewReader("Hello world"))
+	arr := make([]byte, 5)
+	for {
+		n, err := myReader.Read(arr)
+		//fmt.Printf("n = %d err = %v arr = %v\n", n, err, arr)
+		fmt.Printf("%q\n", arr[:n])
+		if err == io.EOF {
+			break
+		}
+	}
 }
